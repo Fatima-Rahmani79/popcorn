8848029864fa7c445094ac8b06ab69281d65db69
Fatima Rahmani <fatima.rahmani79@gmail.com>
2026-08-04 17:48:07 +0430

    Refactor index.js to clean up commented code and restore App component rendering

--- src/index.js
+++ src/index.js
@@ -1,29 +1,18 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
-import StarRating from "./StarRating";
-
-function Test() {
-  const [movieRating, setMovieRating] = useState(0);
-
-  return (
-    <div>
-      <StarRating maxRating={5} color="#084bbe" onSetRating={setMovieRating} />
-      {movieRating ? <p>You rated this movie {movieRating} stars</p> : ""}
-    </div>
-  );
-}
+import "./index.css";
+import App from "./App";
+// import StarRating from "./StarRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating
+    <App />
+    {/* <StarRating
       maxRating={5}
       messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
     />
     <StarRating maxRating={10} size={36} color="#ff6b6b" defaultRating={5} />
-    <Test />
+    */}
   </React.StrictMode>,
 );
